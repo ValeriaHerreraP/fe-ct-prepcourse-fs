7d262fa8d155bb4c7c664ff16be6e6e925a71415
ValeriaHerreraP <valeria.herrera015@pascualbravo.edu.co>
2023-02-07 09:49:17 -0500

    Mi primer commit

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,34 +4,47 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string
 }
+devolverString('Bootcamp Henry')
+
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   return x + y
 }
+suma(4,9)
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   return x - y
 }
+resta (20,7)
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   return x / y
 }
+divide(15,3)
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   return x * y
 }
+multiplica(5,4)
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   return x % y
 }
+obtenerResto(27,3)
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
